f1ddf8fd88a8f654cbb45aa5551de0bab133b1e9
190331-N <190331@hvl.no>
2026-01-22 12:22:25 +0100

    Create 01_opprett_tabeller.sql

---
+++ 01_opprett_tabeller.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS passering CASCADE;
+DROP TABLE IF EXISTS bil CASCADE;
+
+CREATE TABLE bil (
+    regnr           VARCHAR(20) primary key,
+    eier_navn       VARCHAR(100) not null,
+    eier_adresse    VARCHAR(200) not null,
+    epost           VARCHAR(150) not null,
+    telefonnummer   VARCHAR(30) not null,
+);
+
+CREATE TABLE passering (
+    passering_id    BIGSERIAL PRIMARY KEY,
+    regnr           VARCHAR(20) NULL,
+    tidspunkt       TIMESTAMPTZ NOT NULL,
+    bompengebod     VARCHAR(100) NOT NULL,
+    belop           NUMERIC(10,2) NOT NULL CHECK (belop >= 0),
+
+    CONSTRAINT fk_passering_bil
+                       foreign key (regnr)
+                       references bil(regnr)
+                       on update cascade
+                       on delete set null
+);
