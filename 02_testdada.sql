64591301a69433cd8d94e8ec1131a52300b9d5f9
190331-N <190331@hvl.no>
2026-01-27 13:46:57 +0100

    Update 02_testdada.sql

--- 02_testdada.sql
+++ 02_testdada.sql
@@ -1,7 +1,7 @@
-INSERT INTO bil (regnr, eier navn, eier_adresse, epost, telefonnummer) values
+INSERT INTO bil (regnr, eier_navn, eier_adresse, epost, telefonnummer) VALUES
     ('AA10000', 'Testnavn 10', 'Bergen 10', '10@example.com', '1111111'),
     ('BB20000', 'Testnavn 20', 'Bergen 20', '20@example.com', '2222222'),
-    ('CC30000', 'Testnavn 30', 'Bergen 30', '30@example.com', '3333333'),
+    ('CC30000', 'Testnavn 30', 'Bergen 30', '30@example.com', '3333333');
 
 INSERT INTO passering (regnr, tidspunkt, bompengebod, belop) VALUES
     ('AA10000', now() - interval '7 days', 'Bergen vest', 45.00),
