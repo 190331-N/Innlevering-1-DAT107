c1084b7f85bd28e732c54674a6fb5982fa635f90
190331-N <190331@hvl.no>
2026-02-26 11:21:06 +0100

    sdf

--- 02_testadata2.sql
+++ 02_testadata2.sql
@@ -6,5 +6,5 @@ Values
 INSERT INTO medlem (medlemsnr, fornavn, etternavn, telefon, epost, postnr, sted, gateadresse, aktiv_medlem, lokallag_id)
 VALUES
 (100, 'Hans', 'Simmermann', '12345671', 'Hans@test.no', '5010', 'Bergen vest', 'Gate 1', TRUE, 1),
-(101, 'Bo', 'Simmermann','12345672', 'Bo@test.no', '5020', 'Bergen øst', 'Gate 2', TRUE,2),
+(101, 'Bo', 'Karlsen','12345672', 'Bo@test.no', '5020', 'Bergen øst', 'Gate 2', TRUE,2),
 (2001, 'Adrian', 'Simmermann', '12345673', 'Adrian@test.no', '5010', 'Bergen vest', 'Gate 3', TRUE, 1);
