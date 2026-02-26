b7190c23c7a9c1d48b8ebf861a578d98eba12959
HansArne04 <hansarnemid@gmail.com>
2026-02-26 11:47:06 +0100

    Update 01_opprett_tabeller2.sql

--- 01_opprett_tabeller2.sql
+++ 01_opprett_tabeller2.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS Medlem CASCADE;
 DROP TABLE IF EXISTS poststed CASCADE;
+DROP TABLE IF EXISTS lokallag CASCADE;
+DROP TABLE IF EXISTS medlemsavgift CASCADE;
 
 create table Medlem (
-    medlemsnr       BIGSERIAL primary key,
+    medlemsnr       BIGSERIAL   primary key,
     fornavn         varchar(50) NOT NULL,
     etternavn       varchar(50) NOT NULL,
     telefonnr       varchar(20) NOT NULL,
@@ -14,14 +16,25 @@ create table Medlem (
 );
 
 create table poststed (
-    postnr          varchar(4) primary key,
-    sted            varchar(30)
+    postnr          varchar(4)  primary key,
+    sted            varchar(30) NOT NULL
 );
 
 CREATE TABLE lokallag (
     lagnavn         varchar(50) primary key,
-    leder_medlemsnr BIGSERIAL,
-    mote_adresse    varchar(50),
-    postnr          varchar(4) references poststed(postnr)
-)
+    leder_medlemsnr BIGINT      NOT NULL,
+    mote_adresse    varchar(50) NOT NULL,
+    postnr          varchar(4)  NOT NULL references poststed(postnr)
+);
 
+create table medlemsavgift (
+    medlemsnr       BIGINT      NOT NULL references Medlem(medlemsnr),
+    aar             int         NOT NULL,
+    betalt          boolean     NOT NULL,
+    betalt_dato     date,
+    primary key (medlemsnr, aar),
+    CONSTRAINT ck_aar check (aar > 1900 AND aar <= 2026),
+    CONSTRAINT ck_betalt_dato check (
+        (betalt = false And betalt_dato IS NULL)
+        or (betalt = true AND betalt_dato IS NOT NULL))
+)
